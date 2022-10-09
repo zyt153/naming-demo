ffd7aa9b5fe2526cd8c059101f9352bfd6674000
zyutong <zyutong@vmware.com>
2022-10-09 15:24:08 +0800

    update

--- workshop/naming-demo/go.mod
+++ workshop/naming-demo/go.mod
@@ -2,4 +2,8 @@ module github.com/zyt153/naming-demo
 
 go 1.19
 
-require github.com/zyt153/baby-naming v1.0.0 // indirect
+require (
+	github.com/zyt153/baby-naming v1.0.0 // indirect
+	github.com/zyt153/baby-naming/v2 v2.0.0 // indirect
+	github.com/zyt153/baby-naming/v3 v3.0.0 // indirect
+)
--- workshop/naming-demo/main.go
+++ workshop/naming-demo/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"fmt"
 	namingV1 "github.com/zyt153/baby-naming/naming"
-	// namingV2 "github.com/zyt153/baby-naming/v2/naming"
-
+	namingV2 "github.com/zyt153/baby-naming/v2/naming"
+	namingV3 "github.com/zyt153/baby-naming/v3/naming"
 )
 
 func main() {
-	fmt.Println("v1: ", namingV1.CreateBabyName())
-	// fmt.Println("v2: ", namingV2.CreateBabyName(true))
+	fmt.Println("Create a name in naming-v1: ", namingV1.CreateBabyName())
+
+	fmt.Println("Create a boy's name in naming-v2: ", namingV2.CreateBabyName(true))
+	fmt.Println("Create a girl's name in naming-v2: ", namingV2.CreateBabyName(false))
+
+	minLen := 4
+	name, err := namingV3.CreateBabyName(true, minLen)
+	if err == nil {
+		fmt.Printf("Create a boy's name with min length %v in naming-v3: %v", minLen, name)
+	} else {
+		fmt.Println("Invalid input in naming-v3: ", err)
+	}
 }
